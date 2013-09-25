6e160f9b227705132153099edb0cb674bdc284b3
Tim Smith <tim.smith.hdg@gmail.com>
2013-09-25 14:40:47 -0700

    Added Cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,4 +7,24 @@ class Product < ActiveRecord::Base
         with: %r{\.(gif|jpg|png)\Z}i,
         message: 'must be a URL gir GIF, JPG or PNG image.'
     }
+    
+    class Product < ActiveRecord::Base
+        has_many :line_items
+        
+        before_destroy :ensure_not_referenced_by_any_line_item
+        
+        #...
+        
+        private
+        
+            #ensure that there are no lines referencing this product
+            def ensure_not_referenced_by_any_line_item
+                if line_items.empty?
+                    return true
+                else
+                    errors.add(:base, 'Line Items present')
+                    return false
+                end
+            end
+    end
 end
